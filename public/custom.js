bbe8f2c56375f96aba011e5d2aac0143b53e4e01
realanil <46739690+realanil@users.noreply.github.com>
2023-03-23 11:21:37 +0530

    Add public file via upload v1

---
+++ public/custom.js
@@ -0,0 +1,5 @@
+PIXI.utils.destroyTextureCache(true);
+//PIXI.BaseTextureCache[texture].destroy(true);
+PIXI.DisplayObject.prototype.clean = function(remove){    var rmv = remove || false;    for (var i = 0; i < this.children.length; i++) {        this.children[i].clean(true);    }    this.removeChildren();    if(rmv)    {        this.parent.removeChild(this);        if (this instanceof PIXI.Text)            this.destroy(true);        else if(this instanceof PIXI.TilingSprite && this.tilingTexture)            this.tilingTexture.destroy(true);        else if(typeof this.destroy == 'function')            this.destroy(false);    }};
+
+
